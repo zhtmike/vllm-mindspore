b5c4892774f7545b73201a0301b2ab9d5c74163f
moran <moran2@huawei.com>
2025-04-08 11:33:43 +0800

    fix vllm_codecheck tool

--- codecheck_toolkits/vllm_codecheck.sh
+++ codecheck_toolkits/vllm_codecheck.sh
@@ -1,5 +1,7 @@
 pip install -r requirements-lint.txt
 
+RET_FLAG=0
+
 cd ..
 # yapf formats code automatically
 
@@ -11,6 +13,7 @@ fi
 
 if [[ $? -ne 0 ]]; then
   echo "yapf run failed."
+  RET_FLAG=1
 else
   echo "yapf run success."
 fi
@@ -22,6 +25,7 @@ if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' '*.py
 fi
 if [[ $? -ne 0 ]]; then
   echo "codespell check failed."
+  RET_FLAG=1
 else
   echo "codespell check success."
 fi
@@ -33,6 +37,7 @@ if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' '*.py
 fi
 if [[ $? -ne 0 ]]; then
   echo "ruff check failed."
+  RET_FLAG=1
 else
   echo "ruff check success."
 fi
@@ -44,19 +49,24 @@ if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' '*.py
 fi
 if [[ $? -ne 0 ]]; then
   echo "isort fixed failed."
+  RET_FLAG=1
 else
   echo "isort fixed success."
 fi
 
 # mypy check type
+
+PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+
 if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' '*.pyi' &> /dev/null; then
   git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.py' '*.pyi' | xargs -P 5 \
-  mypy --follow-imports skip --python-version 3.9 "$@"
+  mypy --follow-imports skip --python-version "${PYTHON_VERSION}" "$@"
 fi
 if [[ $? -ne 0 ]]; then
   echo "mypy check failed."
+  RET_FLAG=1
 else
   echo "mypy check success."
 fi
 
-cd -
+cd - || exit $RET_FLAG

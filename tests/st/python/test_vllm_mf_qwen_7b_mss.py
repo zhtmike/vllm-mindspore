7ca751ac7ca3c1002652a679215ad031f39dc9f2
zhang_xu_hao1230 <zhangxuhao6@huawei.com>
2025-04-11 15:02:43 +0800

    添加pc特性st用例

--- tests/st/python/test_vllm_mf_qwen_7b_mss.py
+++ tests/st/python/test_vllm_mf_qwen_7b_mss.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# Copyright 2025 Huawei Technologies Co., Ltd
 # Copyright 2024 The vLLM team.
-# Copyright 2024 Microsoft and the HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://wwww.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by application law or agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
--- tests/st/python/test_vllm_mf_qwen_7b_prefix_caching.py
+++ tests/st/python/test_vllm_mf_qwen_7b_prefix_caching.py
@@ -15,66 +15,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""test mf qwen."""
+
+"""test mf qwen prefix caching."""
 import pytest
 import os
 from . import set_env
 env_manager = set_env.EnvVarManager()
-# def env
 env_vars = {
     "MINDFORMERS_MODEL_CONFIG": "./config/predict_qwen2_5_7b_instruct.yaml",
     "ASCEND_CUSTOM_PATH": os.path.expandvars("$ASCEND_HOME_PATH/../"),
     "vLLM_MODEL_BACKEND": "MindFormers",
-    "vLLM_MODEL_MEMORY_USE_GB": "50",
-    "ASCEND_TOTAL_MEMORY_GB": "64",
     "MS_ENABLE_LCCL": "off",
     "HCCL_OP_EXPANSION_MODE": "AIV",
     "ASCEND_RT_VISIBLE_DEVICES": "0,1",
-    "MS_ALLOC_CONF": "enable_vmm:True",
     "LCCL_DETERMINISTIC": "1",
     "HCCL_DETERMINISTIC": "true",
     "ATB_MATMUL_SHUFFLE_K_ENABLE": "0",
     "ATB_LLM_LCOC_ENABLE": "0"
 }
-# set env
 env_manager.setup_ai_environment(env_vars)
 import vllm_mindspore
 from vllm import LLM, SamplingParams
 
 
-class TestMfQwen:
+class TestMfQwen_prefix_caching:
     """
-    Test Qwen.
+    Test qwen7b enable prefix_caching
     """
     @pytest.mark.level0
     @pytest.mark.platform_arm_ascend910b_training
     @pytest.mark.env_single
-    def test_mf_qwen(self):
+    def test_mf_qwen_7b_prefix_caching(self):
         """
-        test case qwen2.5 7B
+        test case qwen_7b_prefix_caching
         """
 
-        # Sample prompts.
+        # First prompts.
         prompts = [
-            "You are a helpful assistant.<｜User｜>将文本分类为中性、负面或正面。 \n文本：我认为这次假期还可以。 \n情感：<｜Assistant｜>\n",
+            "I love Beijing, because it is a city that has so much to offer. I have visited"
         ]
-
+        #second prompts, the second prompt is a continuation of the first prompts, make sure prefix caching work.
+        second_prompts = [
+            "I love Beijing, because it is a city that has so much to offer. I have visited many places"
+        ]
         # Create a sampling params object.
         sampling_params = SamplingParams(temperature=0.0, max_tokens=10, top_k=1)
 
         # Create an LLM.
         llm = LLM(model="/home/workspace/mindspore_dataset/weight/Qwen2.5-7B-Instruct",
+                  max_model_len=8192, block_size=16, enable_prefix_caching=True,
                   gpu_memory_utilization=0.9, tensor_parallel_size=2)
         # Generate texts from the prompts. The output is a list of RequestOutput objects
         # that contain the prompt, generated text, and other information.
         outputs = llm.generate(prompts, sampling_params)
-        except_list=['中性<｜Assistant｜> 这句话']
-        # Print the outputs.
-        for i, output in enumerate(outputs):
-            prompt = output.prompt
-            generated_text = output.outputs[0].text
-            print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+        second_outputs = llm.generate(second_prompts, sampling_params)
+        except_list=[' many times and each time I have found something new']
+        second_except_list=[' to visit, such as the Forbidden City, the']
+        for i, (output, second_output) in enumerate(zip(outputs, second_outputs)):
+            generated_text = output.outputs[i].text
+            print(f"Output1 - Prompt: {prompts[i]!r}, Generated text: {generated_text!r}")
             assert generated_text == except_list[i]
 
-        # unset env
+            second_generated_text = second_output.outputs[i].text
+            print(f"Output2 - Prompt: {second_prompts[i]!r}, Generated text: {second_generated_text!r}")
+            assert second_generated_text == second_except_list[i]
+
         env_manager.unset_all()
